06f8663f8809d09b5160d754e3c90e927fac45c2
Ilya Rybin <rybinil1998@gmail.com>
2024-07-17 17:28:45 +0300

    Создание проекта и подготовка начальной структуры

---
+++ src/modules/App.jsx
@@ -0,0 +1,9 @@
+import 'normalize.css';
+import '../css/App.css';
+import Message from './message';
+
+function App() {
+  return <Message/>
+}
+
+export default App;
---
+++ src/modules/message.jsx
@@ -0,0 +1,10 @@
+function Message() {
+  return <>
+    <div className="message">
+      <h1 className="message_title">Title</h1>
+      <p className="message_text">Text</p>
+    </div>
+  </>;
+}
+
+export default Message;
